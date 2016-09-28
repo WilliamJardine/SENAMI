aaa145b2182828f20fa451ce04a519e8c5cf67d5
William Jardine <williamjardine94@gmail.com>
2016-09-28 22:53:17 +0100

    Updated comments

--- Attack-Scripts/write.py
+++ Attack-Scripts/write.py
@@ -1,5 +1,5 @@
 """
-Writes 1 to DB1.DBX0.1, which turns on auto mode.
+Writes 1 to DB1.DBX0.1, which turns on auto mode (this is specific to Lancaster's ICS testbed setup - will vary system to system!).
 Add the argument -reset to reset bits back to 0 and turn it back off.
 """
 __author__ = 'William Jardine'
